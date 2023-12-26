da5871c28e634cf3ba67ed2077ec737e5b008813
Priya Jha <101475542+mikasa-404@users.noreply.github.com>
2023-12-26 19:35:24 +0530

    Update RestaurantMenu.js
    updated data fetching according to mock data

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -16,16 +16,23 @@ const RestaurantMenu = () => {
 
   if (resInfo === null) return <Shimmer />;
 
+   // const { name, cuisines, costForTwoMessage, city , areaName, avgRating ,totalRatings} =
+  //   resInfo?.cards[0]?.card?.card?.info;
   const { name, cuisines, costForTwoMessage, city , areaName, avgRating ,totalRatings} =
-    resInfo?.cards[0]?.card?.card?.info;
+  resInfo?.info;
+  const itemCards= resInfo?.itemCards;
+  console.log(itemCards)
+    
   // console.log(resInfo?.cards[0]?.card?.card?.info);
-  const itemCardsCheck = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
-  var itemCards  = ((itemCardsCheck)? resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards : resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.categories[0]?.itemCards);
-  if(itemCards==undefined){
-    const cardsCheck = resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
-    if(cardsCheck==undefined) itemCards= resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.categories[0]?.itemCards;
-    else itemCards=cardsCheck;
-  }
+  // const itemCardsCheck = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
+  // var itemCards  = ((itemCardsCheck)? 
+  //resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards : 
+  //resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.categories[0]?.itemCards);
+  // if(itemCards==undefined){
+  //   const cardsCheck = resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
+  //   if(cardsCheck==undefined) itemCards= resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.categories[0]?.itemCards;
+  //   else itemCards=cardsCheck;
+  // }
   
 
   if(itemCards.length!=0) {
